de3cbe565a202fd51aadadac84e91869f2d43470
xjy <3028079152@qq.com>
2025-03-17 11:19:29 +0800

    fix(signal): fix the access problem due to using &Arc instead Arc

--- os/src/syscall/time.rs
+++ os/src/syscall/time.rs
@@ -7,8 +7,8 @@ use crate::{
 
 /// get current time of day
 pub fn sys_gettimeofday(tv: *mut TimeVal) -> isize {
-    let user_check = UserCheck::new();
-    user_check.check_write_slice(tv as *mut u8, core::mem::size_of::<TimeVal>());
+    let _user_check = UserCheck::new();
+    //user_check.check_write_slice(tv as *mut u8, core::mem::size_of::<TimeVal>());
 
     let current_time = get_current_time_ms();
     let time_val = TimeVal {
