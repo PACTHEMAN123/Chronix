8f1a21d53e84165b1f5b61eb2f1da2b9b8ead649
xjy <3028079152@qq.com>
2025-03-19 13:37:07 +0800

    refactor(hal): change way of get trap type

--- os/hal/src/component/trap/riscv64/mod.rs
+++ os/hal/src/component/trap/riscv64/mod.rs
@@ -4,10 +4,16 @@ use riscv::register::{self, scause::{self, Exception, Interrupt, Trap}, sstatus:
 
 use crate::println;
 
-use super::{TrapContextHal, TrapType, FloatContextHal};
+use super::{FloatContextHal, TrapContextHal, TrapType, TrapTypeHal};
 
 core::arch::global_asm!(include_str!("trap.S"));
 
+
+impl TrapTypeHal for TrapType {
+    fn get() -> Self {
+        get_trap_type()
+    }
+}
 
 #[derive(Clone, Copy, Debug)]
 #[repr(C)]
@@ -299,29 +305,6 @@ pub fn set_user_trap_entry() {
     }
 }
 
-#[macro_export]
-macro_rules! define_user_trap_handler {
-    ($fn: ident) => {
-        /// hal_user_trap_handler
-        #[unsafe(export_name = "user_trap_handler")]
-        pub async fn hal_user_trap_handler() {
-            let scause = scause::read();
-            let stval = stval::read();
-
-            let trap_type = match scause.cause() {
-                Trap::Exception(Exception::Breakpoint) => TrapType::Breakpoint,
-                Trap::Exception(Exception::UserEnvCall) => TrapType::Syscall,
-                Trap::Exception(Exception::LoadPageFault) => TrapType::LoadPageFault(stval),
-                Trap::Exception(Exception::StorePageFault) => TrapType::StorePageFault(stval),
-                Trap::Exception(Exception::InstructionPageFault) => TrapType::InstructionPageFault(stval),
-                Trap::Interrupt(Interrupt::SupervisorTimer) => TrapType::Timer,
-                _ => TrapType::Other
-            };
-            
-            $fn(trap_type).await;
-        }
-    };
-}
 
 fn get_trap_type() -> TrapType {
     let scause = scause::read();
@@ -335,13 +318,6 @@ fn get_trap_type() -> TrapType {
         Trap::Exception(Exception::InstructionPageFault) => TrapType::InstructionPageFault(stval),
         Trap::Interrupt(Interrupt::SupervisorTimer) => TrapType::Timer,
         _ => TrapType::Other
-    }
-}
-
-#[unsafe(no_mangle)]
-fn kernel_trap_handler() {
-    unsafe { 
-        super::kernel_trap_handler_for_arch(get_trap_type());
     }
 }
 
