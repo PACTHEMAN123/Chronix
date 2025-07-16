79c5da01a839c6737b8b80df7814a42e9936dc2e
xjy <3028079152@qq.com>
2025-07-16 14:31:17 +0800

    revize(fs): remove origin uts code

--- os/src/fs/mod.rs
+++ os/src/fs/mod.rs
@@ -351,58 +351,6 @@ pub struct Kstat {
     pub st_ctime_nsec: isize,
 }
 
-// Defined in <sys/utsname.h>.
-#[derive(Debug, Clone, Copy)]
-#[repr(C)]
-pub struct UtsName {
-    /// Name of the implementation of the operating system.
-    pub sysname: [u8; 65],
-    /// Name of this node on the network.
-    pub nodename: [u8; 65],
-    /// Current release level of this implementation.
-    pub release: [u8; 65],
-    /// Current version level of this release.
-    pub version: [u8; 65],
-    /// Name of the hardware type the system is running on.
-    pub machine: [u8; 65],
-    /// Name of the domain of this node on the network.
-    pub domainname: [u8; 65],
-}
-
-impl UtsName {
-    pub fn default() -> Self {
-        Self {
-            sysname: Self::from_str("Linux"),
-            nodename: Self::from_str("Linux"),
-            release: Self::from_str("5.19.0-42-generic"),
-            version: Self::from_str(
-                "#43~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Apr 21 16:51:08 UTC 2",
-            ),
-            machine: Self::from_str("RISC-V SiFive Freedom U740 SoC"),
-            domainname: Self::from_str("localhost"),
-        }
-    }
-
-    fn from_str(info: &str) -> [u8; 65] {
-        let mut data: [u8; 65] = [0; 65];
-        data[..info.len()].copy_from_slice(info.as_bytes());
-        data
-    }
-
-    pub fn set_nodename(&mut self, nodename: &[u8]) {
-        //not longer than 64 bytes
-        let len = core::cmp::min(nodename.len(), 64);
-        self.nodename = [0u8; 65];
-        self.nodename[..len].copy_from_slice(&nodename[..len]);
-    }
-
-    pub fn set_domainname(&mut self, domainname: &[u8]) {
-        //not longer than 64 bytes
-        let len = core::cmp::min(domainname.len(), 64);
-        self.domainname = [0u8; 65];
-        self.domainname[..len].copy_from_slice(&domainname[..len]);
-    }
-}
 
 #[derive(Debug, Clone)]
 #[repr(C)]
